9b0a2c0e9ecefb2a9045ec3c6ddf3fd95a3294cd
SeijiroKosakiTMC <seijiroh_kosaki@mail.toyota.co.jp>
2024-10-02 14:41:27 +0900

    Update day2.js

--- day2.js
+++ day2.js
@@ -41,8 +41,8 @@ const unicorns = [
     desc: 'Gentle and kind.'
   },
   {
-    name: 'StareingCourage',
-    img: 'imgs/02_stareingcourage.jpg',
+    name: 'StaringCourage',
+    img: 'imgs/02_staringcourage.jpg',
     desc: 'Young unicorn, brother of NobleBreeze.'
   },
   {
